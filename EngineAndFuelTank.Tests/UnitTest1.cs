59d7cecac23e81fccfc5403718e4b0a5b1bb33d0
iamfranco <kinfaifrancochan@gmail.com>
2022-05-26 07:02:50 +0100

    add more test cases according to codewars

--- EngineAndFuelTank.Tests/UnitTest1.cs
+++ EngineAndFuelTank.Tests/UnitTest1.cs
@@ -10,17 +10,19 @@ namespace EngineAndFuelTank.Tests
         }
 
         [Test]
-        public void TestMotorStartAndStop()
+        public void TestEngineStopsCauseOfNoFuelExactly()
         {
-            var car = new Car();
+            double initialFuelLevel = 0;
+            var car = new Car(initialFuelLevel);
 
             Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+        }
 
-            car.EngineStart();
-
-            Assert.IsTrue(car.EngineIsRunning, "Engine should be running.");
-
-            car.EngineStop();
+        [Test]
+        public void TestEngineStopsCauseOfNoFuelOver()
+        {
+            double initialFuelLevel = -10;
+            var car = new Car(initialFuelLevel);
 
             Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
         }
@@ -38,11 +40,35 @@ namespace EngineAndFuelTank.Tests
         }
 
         [Test]
+        public void TestFuelLevelAllowedUpTo60()
+        {
+            var car = new Car(100);
+
+            Assert.AreEqual(60, car.fuelTankDisplay.FillLevel, "Fuel Level should not exceed 60");
+        }
+
+        [Test]
         public void TestFuelTankDisplayIsComplete()
         {
             var car = new Car(60);
 
             Assert.IsTrue(car.fuelTankDisplay.IsComplete, "Fuel tank must be complete!");
+        }
+
+        [Test]
+        public void TestFuelTankDisplayIsNotComplete()
+        {
+            var car = new Car(50);
+
+            Assert.IsFalse(car.fuelTankDisplay.IsComplete, "Fuel tank must not be complete!");
+        }
+
+        [Test]
+        public void TestFuelTankDisplayIsNotOnReserve()
+        {
+            var car = new Car(10);
+
+            Assert.IsFalse(car.fuelTankDisplay.IsOnReserve, "Fuel tank must not be on reserve!");
         }
 
         [Test]
@@ -54,6 +80,78 @@ namespace EngineAndFuelTank.Tests
         }
 
         [Test]
+        public void TestMotorDoesntStartWithEmptyFuelTank()
+        {
+            double initialFuelLevel = 0;
+            var car = new Car(initialFuelLevel);
+
+            Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+
+            car.EngineStart();
+
+            Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+        }
+
+        [Test]
+        public void TestMotorStartAgainAndStopAgain()
+        {
+            var car = new Car();
+
+            Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+
+            car.EngineStart();
+
+            Assert.IsTrue(car.EngineIsRunning, "Engine should be running.");
+
+            car.EngineStop();
+
+            Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+
+            car.EngineStart();
+
+            Assert.IsTrue(car.EngineIsRunning, "Engine should be running.");
+
+            car.EngineStop();
+
+            Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+        }
+
+        [Test]
+        public void TestMotorStartAndStop()
+        {
+            var car = new Car();
+
+            Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+
+            car.EngineStart();
+
+            Assert.IsTrue(car.EngineIsRunning, "Engine should be running.");
+
+            car.EngineStop();
+
+            Assert.IsFalse(car.EngineIsRunning, "Engine could not be running.");
+        }
+
+        [Test]
+        public void TestNoConsumptionWhenEngineNotRunning()
+        {
+            double initialFuelLevel = 40;
+            var car = new Car(initialFuelLevel);
+
+            Enumerable.Range(0, 1000).ToList().ForEach(_ => car.RunningIdle());
+
+            Assert.AreEqual(initialFuelLevel, car.fuelTankDisplay.FillLevel, "Fuel Level should not change");
+        }
+
+        [Test]
+        public void TestNoNegativeFuelLevelAllowed()
+        {
+            var car = new Car(-2);
+
+            Assert.IsTrue(car.fuelTankDisplay.FillLevel >= 0, "Fuel Level cannot be negative");
+        }
+
+        [Test]
         public void TestRefuel()
         {
             var car = new Car(5);
@@ -61,6 +159,16 @@ namespace EngineAndFuelTank.Tests
             car.Refuel(40);
 
             Assert.AreEqual(45, car.fuelTankDisplay.FillLevel, "Wrong fuel tank fill level!");
+        }
+
+        [Test]
+        public void TestFuelOverMaximum()
+        {
+            var car = new Car(40);
+
+            car.Refuel(30);
+
+            Assert.AreEqual(60, car.fuelTankDisplay.FillLevel, "Fuel Level should not exceed 60");
         }
     }
 }
